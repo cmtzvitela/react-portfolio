84bfcfa8a864d0d8d09f2bc30b586869600526a8
cmtzvitela <89888862+cmtzvitela@users.noreply.github.com>
2024-02-05 19:55:53 -0600

    Few navbar changes

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -21,7 +21,20 @@ const Navbar = () => {
         ))}
       </ul>
       <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(false)} />
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div whileInView={{ x: [300, 0] }} transition={{ duration: 0.85, ease: 'easeOut' }}>
+            <HiX onClick={() => setToggle(false)} />
+            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              <li key={item}>
+                <div />
+                <a href={`#${item}`} onClick={() => setToggle(false)}>
+                  {item}
+                </a>
+              </li>
+            ))}
+          </motion.div>
+        )}
       </div>
     </nav>
   );
